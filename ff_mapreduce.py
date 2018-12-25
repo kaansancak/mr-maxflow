5c7f84120749060ddebe1716e9c8341734f76277
Deniz Ulusel <d.ulusel@gmail.com>
2018-12-25 16:54:35 +0300

    add comments, remove redundant variables

--- ff_mapreduce.py
+++ ff_mapreduce.py
@@ -1,64 +1,62 @@
 import mrjob.job, mrjob.protocol, mrjob.step, sys
 
-# Accumulator is the class that we used for deciding whether to accept
-# augmenting paths in our map-reduce jobs.
-# As long as the edges given to the accumulator does not violate
-# capacity contraints of edges that are already exists in the accumulator, 
-# the acculumator accepts the paths.
 class Accumulator:
     def __init__(self):
         self.edges = dict()
 
     def accept(self, augmenting_path):
+        # assume path is valid, set min_flow to a big number
         valid_path, min_flow = (True, 1000000)
 
-        # Check whether the edge violates the capacity contrainsts
-        # Calculate the flow amount that can be puushed through the path
+        # edge[0] = destination vertex
+        # edge[1] = edge_id (vertex1_id, vertex2_id)
+        # edge[2] = flow on edge
+        # edge[3] = capacity of edge
+
         for edge in augmenting_path:
+            # attempt to get edge flow by id
             edge_option = self.edges.get(edge[1], None)
 
             if edge_option == None:
+                # if edge isn't already in path, init flow to 0
                 accumulated_flow = 0
             else:
+                # else init flow to existing flow
                 accumulated_flow = edge_option
 
+            # set residue to (capacity) - (accumulated_flow) - (flow on edge)
             residue = edge[3]
             residue -= accumulated_flow
             residue -= edge[2]
 
+            # if residue is non positive, augmenting path is invalid
             valid_path = False if residue <= 0 else valid_path
+            # if residue is positive and smaller than min_flow on path, set min_flow to residue
             min_flow = residue if (residue > 0 and residue < min_flow) else min_flow
 
-        # Update the edges in the accumulator
         if (valid_path == False):
+            # reject path if invalid
             return False
         else:
             for edge in augmenting_path:
-                e_v = edge[0]
-                e_id = edge[1]
-                e_f = edge[2]
-                e_c = edge[3]
-                r = edge[1].split(",")
-                e_r = str(r[1]) + "," + str(r[0])
-
+                # check if edge is in path
                 id_option = self.edges.get(edge[1], None)
+                # if it is not, set flow on edge to min_flow
+                # else set flow on edge to (flow on edge + min_flow)
                 self.edges[edge[1]] = self.edges[edge[1]] + min_flow if id_option != None else min_flow
             return True
 
-# We define the maximum number of source exess paths
-# that can be accepted by the accumulator as 10
+# accumulators don't accept more source excess paths than this
 MAX_PATHS = 10
 
+### helpers for map function ###
 
-# Check whether the edges forms a cycle
-# and make sure that adding a new edge does not creates a cycle
+# check cycle before adding a new edge to path
 def edge_forms_cycle(new_edge, path):
     return any(map(lambda edge: edge[0] == new_edge[0], path))
 
-# Update the flow of the augmented edges
-# Check for the capacity constraint
-# if the flow value is equal or greater than the
-# capacity add the edge to the satured edges
+# update flow of edge in augmented_edges
+# add edge to saturated_edges if flow >= capacity
 def update_edge(edge, augmented_edges, saturated_edges):
     edge_option = augmented_edges.get(edge[1], None)
 
@@ -68,23 +66,24 @@ def update_edge(edge, augmented_edges, saturated_edges):
     if edge_option!= None and edge[2] >= edge[3] and saturated_edges.count(edge[1]) == 0:
         saturated_edges.append(edge[1])
 
-
+### MRJob job definition ###
 class MRFlow(mrjob.job.MRJob):
-    # We decided to use JSON formatting in our
-    # input-output formats
     OUTPUT_PROTOCOL, INPUT_PROTOCOL = (mrjob.protocol.JSONProtocol, mrjob.protocol.JSONProtocol)
 
     def mapper(self, u, node_info):
         saturated_edges = list()
 
-        # augmented_edges contains edges that are
-        # augmented from previous round of the map-reduce job
+        # intermediate result from previous job
+        # contains edges that were augmented before
         augmented_edges = node_info[-1]
         del node_info[-1]
 
-        # update edges in sink excess, source excess and edges
-        # with augmented edges
-        # note that update_edge addes saturated edges to satured_edges
+        # node_info[0]: S_u source excess paths
+        # node_info[1]: T_u sink excess paths
+        # node_info[2]: E_u list of edges connecting u to neighbors
+
+        # update source and sink excess paths and edges from u according to augmented edges
+        # and add saturated edges
         for edge in node_info[2]:
             update_edge(edge, augmented_edges, saturated_edges)
         for path in node_info[0]:
@@ -94,7 +93,7 @@ class MRFlow(mrjob.job.MRJob):
             for edge in path:
                 update_edge(edge, augmented_edges, saturated_edges)
 
-        # remove satured excess paths from source excess and sink excess
+        # remove source and sink excess paths that are saturated
         for e_id in saturated_edges:
             for path in node_info[0]:
                 if path.count(e_id) > 0:
@@ -104,21 +103,20 @@ class MRFlow(mrjob.job.MRJob):
                 if path.count(e_id) > 0:
                     node_info[1].remove(path)
 
-        # create a accumulator to decide whether the accept new augmenting edeges
+        # try to connect source excess paths to sink excess paths
         accumulator = Accumulator()
 
-        # combine sink and source excess paths if possible
         for source_path in node_info[0]:
             for sink_path in node_info[1]:
                 if accumulator.accept(source_path + sink_path):
                     yield ("t", [[source_path + sink_path], [], []])
 
-        # Seed sink vertex to neigbouring vertices
+        # regenerate neighbours of s to search new source excess paths
         if u == "s":
             for edge in node_info[2]:
                 yield (edge[0], [[[[edge[0], edge[1], edge[2], edge[3]]]], list(), list()])
 
-        # extend the source excess paths
+        # extend source excess paths
         if len(node_info[0]) > 0:
             for edge in node_info[2]:
                 e_v, e_f, e_c = edge[0], edge[2], edge[3]
@@ -127,32 +125,26 @@ class MRFlow(mrjob.job.MRJob):
                         if not edge_forms_cycle(edge, source_path):
                             yield (e_v, [[source_path + [edge]], list(), list()])
 
-        # emit the intermediate values
         yield (u, [node_info[0], node_info[1], node_info[2]])
 
     def reducer(self, u, values):
-
-        # initialize acculumulators in order to decide whether to accept paths
+        # initialize new Accumulators
         A_p = Accumulator()
         A_s = Accumulator()
         A_t = Accumulator()
 
-        S_m = list()
-        T_m = list()
-        S_u = list()
-        T_u = list()
-        E_u = list()
+        S_u = list() # source excess paths
+        T_u = list() # sink excess paths
+        E_u = list() # list of edges connecting u to neighbours
 
+        # val[0] = source excess paths to vertex
+        # val[1] = sink excess paths to vertex
+        # val[2] = list of edges connecting vertex to neighbour
         for val in values:
-
-            # Get the sink excess, source excess and edges values
-            # for master vertex with key u
             if len(val[2]) > 0:
-                S_m = val[0]
-                T_m = val[1]
                 E_u = val[2]
 
-            # merge and filter the paths
+            # merge and filter S_v
             for se in val[0]:
                 if u == "t":
                     A_p.accept(se)
@@ -163,18 +155,12 @@ class MRFlow(mrjob.job.MRJob):
                 if len(T_u) < MAX_PATHS and A_t.accept(te):
                     T_u.append(te)
 
-        # set the counter values
+        # initalize counter
         counter_init = [("move", "source", 0), ("move", "source", 0), ("move", "source", len(S_u))]
-        
-        # increment the counters of the current running job
         for item in counter_init:
             self.increment_counter(item[0], item[1], item[2])
 
-        # emit results
         is_sink = u== "t"
-
-        # if the master vertex is the sink vertex
-        # also append the resulting values to A_p
         if is_sink:
             key = "A_p"
             value = A_p.edges
